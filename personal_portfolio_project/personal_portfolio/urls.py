68adfe396474622344a728d954d7b829c6c0c34f
Kvezac <Kvezak@gmail.com>
2022-05-01 01:40:03 +0300

    blog: admin.py, models.py, views.py. personal_portfolio: urls.py.

--- personal_portfolio_project/personal_portfolio/urls.py
+++ personal_portfolio_project/personal_portfolio/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls import include
 from portfolio import views
 
 urlpatterns = [
